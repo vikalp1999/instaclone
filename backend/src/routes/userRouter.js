a19bc4f76180c22eb40b23e3483a3ea8f2c74af9
vikalp1999 <shandilyavikalp@gmail.com>
2023-03-18 23:00:35 +0530

    create route for user

---
+++ backend/src/routes/userRouter.js
@@ -0,0 +1,20 @@
+const router = require('express').Router()
+const { searchUser, getUser, follow, unfollow, suggestionsUser } = require('../controller/userCtrl')
+const auth = require("../middleware/auth")
+
+
+
+router.get('/search', auth, searchUser)
+
+router.get('/user/:id', auth,getUser)
+
+router.patch('/user', auth, updateUser)
+
+router.patch('/user/:id/follow', auth, follow)
+router.patch('/user/:id/unfollow', auth, unfollow)
+
+router.get('/suggestionsUser', auth, suggestionsUser)
+
+
+
+module.exports = router
