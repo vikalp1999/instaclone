98d48cd2b6eb1a7cff9a421d745e0dc48d5ada26
vikalp1999 <shandilyavikalp@gmail.com>
2023-03-20 09:07:39 +0530

    deploy frontend on vercel

--- frontend/src/utils/fetchData.js
+++ frontend/src/utils/fetchData.js
@@ -1,35 +1,35 @@
 import axios from 'axios'
 
 export const getDataAPI = async (url, token) => {
-    const res = await axios.get(`/api/${url}`, {
+    const res = await axios.get(`https://instaclone-j21j.onrender.com/api/${url}`, {
         headers: { Authorization: token}
     })
     return res;
 }
 
 export const postDataAPI = async (url, post, token) => {
-    const res = await axios.post(`http://localhost:5000/api/${url}`, post, {
+    const res = await axios.post(`https://instaclone-j21j.onrender.com/api/${url}`, post, {
         headers: { Authorization: token}
     })
     return res;
 }
 
 export const putDataAPI = async (url, post, token) => {
-    const res = await axios.put(`/api/${url}`, post, {
+    const res = await axios.put(`https://instaclone-j21j.onrender.com/api/${url}`, post, {
         headers: { Authorization: token}
     })
     return res;
 }
 
 export const patchDataAPI = async (url, post, token) => {
-    const res = await axios.patch(`/api/${url}`, post, {
+    const res = await axios.patch(`https://instaclone-j21j.onrender.com/api/${url}`, post, {
         headers: { Authorization: token}
     })
     return res;
 }
 
 export const deleteDataAPI = async (url, token) => {
-    const res = await axios.delete(`/api/${url}`, {
+    const res = await axios.delete(`https://instaclone-j21j.onrender.com/api/${url}`, {
         headers: { Authorization: token}
     })
     return res;
